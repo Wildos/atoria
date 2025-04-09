8a1e9ef14bbe4e6b3fda3a99b1dac0ad7fae5651
Thomas FERNANDES <fernandes.thomas.student@gmail.com>
2025-04-09 18:10:55 +0200

    up to 0.3.4

--- module/models/items/actable-items/actable-modifier-item.mjs
+++ module/models/items/actable-items/actable-modifier-item.mjs
@@ -1,16 +1,21 @@
 import * as atoria_models from "../../module.mjs";
 
 export default class AtoriaActableModifierItem extends atoria_models.AtoriaItemBase {
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    const schema = super.defineSchema();
 
-    static defineSchema() {
-        const schema = super.defineSchema();
+    schema.cost = atoria_models.helpers.defineCostField();
 
-        schema.cost = atoria_models.helpers.defineCostField();
+    schema.limitation = atoria_models.helpers.defineTimePhaseLimitation();
 
-        schema.limitation = atoria_models.helpers.defineTimePhaseLimitation();
+    schema.restriction = new fields.StringField({
+      required: true,
+      label: "ATORIA.Ruleset.Actable.Restriction",
+    });
 
-        schema.skill_alteration = atoria_models.helpers.defineSkillAlteration();
+    schema.skill_alteration = atoria_models.helpers.defineSkillAlteration();
 
-        return schema;
-    }
+    return schema;
+  }
 }
--- module/models/items/actable-items/incantatory-addition-item.mjs
+++ module/models/items/actable-items/incantatory-addition-item.mjs
@@ -1,7 +1,38 @@
 import * as atoria_models from "../../module.mjs";
+import * as utils from "../../../utils/module.mjs";
 
 export default class AtoriaIncantatoryAdditionItem extends atoria_models.AtoriaActableModifierItem {
-    static defineSchema() {
-        return super.defineSchema();
-    }
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    const schema = super.defineSchema();
+
+    schema.require_immobile = new fields.BooleanField({
+      required: true,
+      initial: false,
+      label: "ATORIA.Ruleset.Actable.Require_immobile",
+    });
+    schema.travel = new fields.StringField({
+      required: true,
+      label: "ATORIA.Ruleset.Actable.Travel",
+    });
+    schema.somatic = new fields.StringField({
+      required: true,
+      nullable: false,
+      blank: false,
+      choices: utils.ruleset.actable.somatic_types,
+      initial: Object.keys(utils.ruleset.actable.somatic_types)[0],
+      label: "ATORIA.Ruleset.Actable.Somatic",
+    });
+
+    schema.require_verbal = new fields.BooleanField({
+      required: true,
+      initial: false,
+      label: "ATORIA.Ruleset.Actable.Require_verbal",
+    });
+    schema.material = new fields.StringField({
+      required: true,
+      label: "ATORIA.Ruleset.Actable.Material",
+    });
+    return schema;
+  }
 }
